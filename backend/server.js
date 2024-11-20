d3e9bb122459b0837435cc658f68d038f3ddde20
BHOR PRAKASH <101253263+bhorprakash@users.noreply.github.com>
2024-11-20 23:14:07 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'bunty',
+    password: '',
     database: 'quizdb',
 });
 
